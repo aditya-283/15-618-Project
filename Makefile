d66064f55b8fe10a335f0f01976ab18f75873d08
kbhat2 <kbhat2@andrew.cmu.edu>
2022-04-09 18:59:10 -0400

    Collective Average

--- Makefile
+++ Makefile
@@ -10,8 +10,8 @@ all: mpirun output
 mpirun : mpirun.o mpi.o socket.o rio.o
 	$(CXX) $(CXXFLAGS) mpirun.o mpi.o socket.o rio.o -o mpirun
 
-output : helloworld.o mpi.o socket.o rio.o
-	$(CXX) $(CXXFLAGS) helloworld.o mpi.o socket.o rio.o -o output
+output : average.o mpi.o socket.o rio.o
+	$(CXX) $(CXXFLAGS) average.o mpi.o socket.o rio.o -o output
 
 # Object Files
 mpirun.o : mpirun.cpp 
@@ -26,8 +26,8 @@ rio.o : rio.cpp
 socket.o : socket.cpp
 	$(CXX) $(CXXFLAGS) -c socket.cpp
 
-helloworld.o : helloworld.cpp
-	$(CXX) $(CXXFLAGS) -c helloworld.cpp 
+average.o : average.cpp
+	$(CXX) $(CXXFLAGS) -c average.cpp 
 
 
 clean : 
---
+++ average.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+#include <unistd.h>
+#include <string.h>
+
+#define ROOT_PROC       0
+#define ARRAY_SIZE      1000000
+
+int main(int argc, char* argv[]) {
+    int nProc=0, procId=0;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(&procId);
+    MPI_Comm_size(&nProc);
+    
+    long int chunkSize = ARRAY_SIZE / nProc;
+    int *intArray = (int *) malloc(sizeof(int) * chunkSize);
+
+    for (int i = 0; i < chunkSize; i ++) {
+        intArray[i] = rand() % 100;
+    }
+
+    long int sum = 0;
+    for (int i = 0; i < chunkSize; i ++) {
+        sum += intArray[i];
+    }    
+    double localAvg = (double) sum / chunkSize;
+
+    if (procId != ROOT_PROC) {
+        MPI_Send(&localAvg, 1, MPI_DOUBLE, ROOT_PROC);
+    } else {
+        double *avgArray = (double *) malloc(sizeof(double) * nProc);
+        avgArray[ROOT_PROC] = localAvg;
+        MPI_Status status;
+        for (int sourceProc = 0; sourceProc < nProc; sourceProc ++) {
+            if (sourceProc == ROOT_PROC) continue;
+            MPI_Recv(&avgArray[sourceProc], 1, MPI_DOUBLE, sourceProc, &status);
+        }
+
+        double finalAvg = 0;
+        for (int i = 0; i < nProc; i ++) {
+            finalAvg += avgArray[i];
+        }
+        finalAvg /= nProc;
+
+        printf("Final Average is %lf\n", finalAvg);
+    }
+
+    MPI_Finalize();
+}
